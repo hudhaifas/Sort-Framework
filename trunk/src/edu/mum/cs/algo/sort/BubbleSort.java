f6c3ca2c0d8a1b7bb5dd9c41013903aa01f55bf4
hudhaifa.shatnawi@gmail.com <hudhaifa.shatnawi@gmail.com@06d89426-ef3f-582f-2348-a16343e3b42a>
2014-01-16 04:27:50 +0000

    Updates the documentation

--- trunk/src/edu/mum/cs/algo/sort/BubbleSort.java
+++ trunk/src/edu/mum/cs/algo/sort/BubbleSort.java
@@ -16,6 +16,10 @@
 package edu.mum.cs.algo.sort;
 
 /**
+ * This class implements the Bubble sort algorithm which starts finding the largest value and moving it to the
+ * end of the array. This algorithm offers O(n^2) complexity.
+ *
+ * The Bubble sort is the opposite of the Shuffle sort, it starts sorting the largest value.
  *
  * @author Hudhaifa Shatnawi <hudhaifa.shatnawi@gmail.com>
  * @version 1.0, Sep 24, 2013 - 5:44:14 PM
@@ -30,6 +34,7 @@ public class BubbleSort
 
     @Override
     public void sort() {
+        // Iterates all elements and swap the greatest element to the end of the array.
         for (int i = arr.length; --i >= 0;) {
             for (int j = 0; j < i; j++) {
                 if (isGreater(arr[j], arr[j + 1])) {

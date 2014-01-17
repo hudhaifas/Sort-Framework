6365c3fd5dba46a717433efa61351bb32e94bce2
hudhaifa.shatnawi@gmail.com <hudhaifa.shatnawi@gmail.com@06d89426-ef3f-582f-2348-a16343e3b42a>
2014-01-17 05:32:07 +0000

    First version of the Parallel implementation of the Merge sort algorithm.

--- trunk/src/edu/mum/cs/parallel/ParallelIMergeSort.java
+++ trunk/src/edu/mum/cs/parallel/ParallelIMergeSort.java
@@ -20,20 +20,20 @@ import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
 
 /**
- * This class implements a parallel version of the Merge sort algorithm which is a fork each task to work in
- * different process. This algorithm offers O(n log(n)) complexity in a single processor machine but it is
- * typically faster than traditional Merge sort implementations in the big data sets and multi processor
- * machines.
+ * This is a parallel implementation of the IMerge sort algorithm which is a combination of merge sort
+ * algorithm and insertion sort algorithm based on the array size. This algorithm offers O(n log(n))
+ * complexity in a single processor machines but it is typically faster than traditional Merge sort
+ * implementations in the big data sets and multi processor machines.
  *
  * @author Hudhaifa Shatnawi <hudhaifa.shatnawi@gmail.com>
  * @version 1.0, Oct 13, 2013 - 10:45:54 PM
  */
-public class ParallelMergeSort
+public class ParallelIMergeSort
         extends Sort {
 
     @Override
     public String getName() {
-        return "Parallel MergeSort";
+        return "Parallel IMergeSort";
     }
 
     @Override
@@ -51,6 +51,12 @@ public class ParallelMergeSort
         finish();
     }
 
+    private void updateHelper(int left, int right) {
+        for (int i = left; i <= right; i++) {
+            helper[i] = arr[i];
+        }
+    }
+
     /**
      *
      */
@@ -64,16 +70,6 @@ public class ParallelMergeSort
 
         @Override
         protected void compute() {
-            divide(left, right);
-        }
-
-        /**
-         * Divides the array into two equaled sub arrays.
-         *
-         * @param left the first index of the array or sub array
-         * @param right the last index of the array or sub array
-         */
-        private void divide(int left, int right) {
             int length = right - left;
 
             if (length < 1) {
@@ -86,13 +82,34 @@ public class ParallelMergeSort
             int middle = left + length / 2;
 
             if (length <= SINGLETHREAD_THRESHOLD) {
-                divide(left, middle);
-                divide(middle + 1, right);
+                insertionSort(left, right);
             } else {
                 invokeAll(new ParallelDivide(left, middle), new ParallelDivide(middle + 1, right));
             }
 
             merge(left, middle, right);
+        }
+
+        /**
+         * Sort the array or sub array by insertion sort.
+         *
+         * @param left the start index
+         * @param right the end index
+         */
+        private void insertionSort(int left, int right) {
+            int temp;
+
+            for (int i = left + 1; i <= right; i++) {
+                temp = arr[i];
+                int j;
+                for (j = i; j > left && isLess(temp, arr[j - 1]); j--) {
+                    swap(j, j - 1);
+                    notifyCursor(i, j);
+                }
+                arr[j] = temp;
+                notifyCursor(i);
+            }
+            updateHelper(left, right);
         }
 
         /**
@@ -130,9 +147,7 @@ public class ParallelMergeSort
                 notifyCursor(targetCount, rightCount);
             }
 
-            for (int i = left; i <= right; i++) {
-                helper[i] = arr[i];
-            }
+            updateHelper(left, right);
         }
 
         private final int left;
